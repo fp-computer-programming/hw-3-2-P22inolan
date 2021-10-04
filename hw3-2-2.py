e914f644cb4b16b4838568e9dc96b91edc990a1c
P22inolan <p22inolan@fairfieldprep.org>
2021-10-04 09:45:53 -0400

    HW Submit 10/4

--- hw3-2-2.py
+++ hw3-2-2.py
@@ -1,11 +1,17 @@
 # Author: IBN (AMDG) 9/30/2021
 
-weight = int(input("What is your weight? "))
-height = int(input("What is your height? "))
+weight = int(input("What is your weight in pounds? "))
+height = int(input("What is your height in inches? "))
 
 bmi = (weight / height / height) * 703
 
 if bmi < 19:
     print("You are underweight.")
-elif bmi <=25 :
+elif bmi <= 25:
     print("You are healthy.")
+elif bmi <= 30:
+    print("You are overweight.")
+elif bmi <= 40:
+    print("You are Obese.")
+else:
+    print("You are Extremely Obese.")
